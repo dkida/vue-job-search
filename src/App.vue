c095e6483cdd3cb5f54faf9edec81dd6cd0c6462
dkida <danielkida@hotmail.com>
2023-02-18 23:52:41 +0100

    Added navigation buttons and navigation test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1 class="mt-8 text-center text-6xl">Hello Vue</h1>
   <main-nav />
 </template>
 
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,22 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Kida Careers");
     expect(companyName).toBeInTheDocument();
   });
+
+  it("displays menu items for navigation", () => {
+    const navigationItemsNames = [
+      "Teams",
+      "Locations",
+      "Life at Kida Careers",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ];
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+
+    expect(navigationMenuTexts).toEqual(navigationItemsNames);
+  });
 });
